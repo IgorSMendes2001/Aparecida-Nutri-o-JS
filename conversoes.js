f94cffd81dec34d905f4ce0eea202d3ebb00d251
Igor Mendes <igor07082001@gmail.com>
2022-03-15 00:52:02 -0300

    Confirmar

---
+++ conversoes.js
@@ -0,0 +1,18 @@
+//conversão implícita
+const numero=3;
+const nString="3";
+
+console.log(numero==nString);
+console.log(numero+nString);
+//conversão explícita
+//Number()
+//String()
+console.log(Number(nString)+numero);
+
+//operador ternario:
+const idadeMinima=18;
+const idadeDoCliente=19;
+console.log(idadeDoCliente>=idadeMinima?"Parabéns,você poderá fazer autoescola e ser contratado para estagiar conosco!":"Oh nooo,você é muito noviço ainda,espere mais um pouco champs");
+
+
+//Template Strings:
--- js/type-number.js
+++ js/type-number.js
@@ -2,10 +2,21 @@ const numero=23;
 const primeiroNumero=1;
 const segundoNumero=3;
 const resultado=numero+primeiroNumero;
-console.log(resultado);
+/* console.log(resultado); */
 const resultado2= primeiroNumero/3.3;
-console.log(resultado2);
+/* console.log(resultado2); */
 
 const leia="Esqueceram de mim!";
 
-console.log(primeiroNumero===segundoNumero);
+/* console.log(primeiroNumero===segundoNumero); */
+
+let altura= 1.20;
+let comprimento=8;
+let areaRetangulo=altura*comprimento;
+console.log(areaRetangulo);
+
+let lado=5;
+let areaQuadrado=lado*lado;
+console.log(areaQuadrado);
+let areaTriangulo=areaRetangulo/2;
+console.log(areaTriangulo);
